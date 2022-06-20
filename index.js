d4db645fd1d1ffd6e8124748b5a496c40e39d352
Shane99dt <dushen1999thilaksha@gmail.com>
2022-06-20 16:57:37 +0200

    exercise

--- index.js
+++ index.js
@@ -27,15 +27,34 @@ console.log(`
 `)
 
 
+const array = [1, 2, 3, 4, 5]
 
+array.forEach(element => {
+  if(999 % element == 0){
+    console.log(`${element} completely devides 999`)
+  }
+});
 
+array.forEach(element => {
+  if(25 % element == 0){
+    console.log(`${element} completely devides 25`)
+  }
+});
 
+array.forEach(element => {
+  if(190280 % element == 0){
+    console.log(`${element} completely devides 190280`)
+  }
+});
 
+array.forEach(element => {
+  if(47 % element == 0){
+    console.log(`${element} completely devides 47`)
+  }
+});
 
-// const array = [1, 2, 3, 4, 5]
-
-// for (var i in array){
-//   if( 25 % i == 0 ){
-//     console.log(i)
-//   }
-// }
+array.forEach(element => {
+  if(691 % element == 0){
+    console.log(`${element} completely devides 691`)
+  }
+});
