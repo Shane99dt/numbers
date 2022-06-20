8843a37d673a51c1b25d39c63ebe2935a9ece380
Shane99dt <dushen1999thilaksha@gmail.com>
2022-06-20 16:44:44 +0200

    exercise

---
+++ index.js
@@ -0,0 +1,41 @@
+const integer = 102
+const float = 13.9
+
+console.log(`
+  ${integer},
+  ${float}
+`)
+
+const basic = 34
+const stringified = basic.toString()
+console.log(stringified)
+console.log(typeof(stringified))
+
+
+const num = 1.5
+const rounded = Math.round(num)
+console.log(rounded)
+
+const test = 12
+const bis = 5
+console.log(`
+  addition : ${test + bis},
+  soustraction : ${test - bis},
+  division : ${test / bis},
+  multiplication : ${test * bis},
+  modulo: ${test % bis}
+`)
+
+
+
+
+
+
+
+// const array = [1, 2, 3, 4, 5]
+
+// for (var i in array){
+//   if( 25 % i == 0 ){
+//     console.log(i)
+//   }
+// }
